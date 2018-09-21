0e10114e4ca11c899c4f9a9669a9eed02d4e3f82
Colin Ries <colin.ries@gmail.com>
2018-09-21 18:21:39 +0100

    fixed interpreter

--- game.py
+++ game.py
@@ -23,7 +23,7 @@ gridWidth = width / cellSize
 
 white = (255, 255, 255)
 red = (255, 0, 0)
-yellow = (128, 255, 0)
+yellow = (255, 255, 0)
 black = (0, 0, 0)
 
 move_timedelta = 1000000.0 / moves_per_second # in microseconds
